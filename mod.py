b0867b508d2d823a5f1f54273d62c91fb9a3476d
JoumeneMami <joumene.mami09@em-normandie.fr>
2024-10-21 16:22:41 +0100

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,10 @@
+# 7.4.1 Create a python module mod.
+# In mod.py, create a function psum(number1, number2) that takes two arguments and prints the sum of them. Assume the input arguments to be numbers.
+# In mod.py, create a function pmultiply(number1, number2) that takes two arguments and prints the product of them. Assume the input arguments to be numbers.
+
+def psum(number1, number2):
+    print(number1 + number2)
+
+def pmultiply(number1, number2):
+    print(number1 * number2)
+    
